823ac37406e768e844d1ca2d35afca9f5bd60ad4
Simon Quigley <tsimonq2@ubuntu.com>
2019-03-04 19:26:49 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# PPA Britney
+
+This is a combination of code from [Ubuntu's britney2](https://git.launchpad.net/~ubuntu-release/britney/+git/britney2-ubuntu) and [Bileto](https://git.launchpad.net/bileto/) which sets up a full Britney instance to be run with PPAs instead of the actual archive. This is currently being used by the [Lubuntu CI Team](https://launchpad.net/~lubuntu-ci).
+
+The original code belongs to their respective authors, either Canonical Ltd or individual copyright holders. When a copyright holder is not specified, please refer to the respective repositories to get information.
---
+++ config.sh
@@ -0,0 +1,19 @@
+export RELEASE="disco"
+
+export LP_TEAM="lubuntu-ci"
+export SOURCE_PPA="unstable-ci-proposed"
+export SOURCE_PPA_URL="http://ppa.launchpad.net/$LP_TEAM/$SOURCE_PPA/ubuntu/dists/$RELEASE/main"
+export DEST_PPA="unstable-ci"
+export DEST_PPA_URL="http://ppa.launchpad.net/$LP_TEAM/$DEST_PPA/ubuntu/dists/$RELEASE/main"
+
+export MAIN_ARCHIVE="http://archive.ubuntu.com/ubuntu/dists"
+export PORTS_ARCHIVE="http://ports.ubuntu.com/dists"
+export ARCHES="i386 amd64"
+export PORTS_ARCHES="armhf arm64 ppc64el s390x"
+
+export BRITNEY_LOC="britney"
+export BRITNEY_DATADIR="britney_data"
+export BRITNEY_OUTDIR="britney_output"
+export BRITNEY_HINTDIR="britney_hints"
+export BRITNEY_CACHE="britney_cache"
+export BRITNEY_TIMESTAMP=$(date +'%Y%m%d%H%M')
